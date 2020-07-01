7b34b27ad8fcef7edb9e492749d4e3c7c688bc2a
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-06-30 21:06:34 -0600

    completed showQuestion function

--- Assets/script.js
+++ Assets/script.js
@@ -21,77 +21,36 @@ var questions = [
 {
     question: "What is the HTML tag that will output the largest header?",
     answer: ["h6", "h3", "h1", "h0"],
-    correctAnswer: 2,
+    correctAnswer: "2",
 },
 {
     question: "How do you indicate an id in CSS",
     answer: [".", "id", "-", "#"],
-    correctAnswer: 3,
+    correctAnswer: "3",
 },
 {
     question: "How do you start an HTML document?",
     answer: ["html", "<!Doctype>", "!Doctype", "main"],
-    correctAnswer: 1,
+    correctAnswer: "1",
 },
 {
     question: "What character points to the entire document in CSS?",
     answer: ["*", "-", "$", "~"],
-    correctAnswer: 0,
+    correctAnswer: "0",
 }
 ]
 
 
 
 // * Functions
-function showQuestion() {
-    var nextQuestion = questions[questionIndex];
+// startTimer: begins timer at 75 seconds (secondsLeft) and counts down by 1 and stops at 0
+// start quiz- hides intro div, starts timer, and begins questions
+// questions: display question and answer choices from questions array. it will need to loop through each answer and create a list item and button for each answer choice. it will also need to check answer to see if it is the correct answer. An incorrect answer decreases the time by 10 seconds. any answer advances to the next question
+// stop quiz: the quiz is over when the timer reaches 0 or when there are no more questions. Function will need to hide the questions div and show the high scores page
+// get scores: display high scores from local storage
+// save scores: saves scores to local storage
 
-    var questionDiv = document.querySelector("#question");
-    var answerDiv = document.querySelector("#answers");
-    var showAnswer = document.querySelector("#showAnswer");
-
-    questionDiv.textContent = nextQuestion.question;
-
-    var answerList = document.createElement("ul");
-    var listItem;
-    var answerButton;
-
-    function checkAnswer() {
-
-        // ! WORK THIS
-        console.log(nextQuestion.answer[0]);
-        console.log(nextQuestion.correctAnswer);
-            
-        for (var i = 0; i < nextQuestion.answer.length; i++) {
-            if (nextQuestion.answer[i] === nextQuestion.correctAnswer) {
-                showAnswer.textContent = "Correct";
-        } 
-        else {
-            showAnswer.textContent = "Wrong";
-            // secondsLeft-= 10;
-        }
-        }
-    }   
-        
-    
-
-    
-    for (var i = 0; i < nextQuestion.answer.length; i++) {
-        listItem = document.createElement("li");
-        answerButton = document.createElement("button");
-        answerButton.textContent = nextQuestion.answer[i];
-        answerButton.addEventListener("click", checkAnswer);
-
-        listItem.appendChild(answerButton);
-        answerList.appendChild(listItem);
-    }
-    
-    answerDiv.appendChild(answerList);
-
-    
-};
-
-
+// * Step 1
 function startTimer() {
     
     var timeInterval = setInterval(function() {
@@ -115,13 +74,66 @@ function startTimer() {
 
 }
 
-
 function startQuiz() {
     intro.classList.add("hidden");
     
     startTimer();
     showQuestion();
 }
+
+
+
+function showQuestion() {
+    var nextQuestion = questions[questionIndex];
+    var questionDiv = document.querySelector("#question");
+    var answerDiv = document.querySelector("#answers");
+    var showAnswer = document.querySelector("#showAnswer");
+    var answerList = document.createElement("ul");
+    var listItem;
+    var answerButton;
+    
+    questionDiv.textContent = nextQuestion.question;
+    
+    for (var i = 0; i < nextQuestion.answer.length; i++) {
+        listItem = document.createElement("li"); // create list item
+        answerButton = document.createElement("button"); // create button that is inside list item
+        answerButton.setAttribute("data-index", i); // gives each list item an index
+        answerButton.textContent = nextQuestion.answer[i]; // populates button with each answer in array
+
+        // checks if answer is correct
+        answerButton.addEventListener("click",  function(event) {
+            var el = event.target;
+            var index = el.getAttribute("data-index");
+            console.log(index)
+            console.log(nextQuestion.correctAnswer)
+            
+        if (nextQuestion.correctAnswer !== index) {
+            showAnswer.textContent = "Wrong";
+            secondsLeft-=5;
+            answerDiv.innerHTML = " ";
+        } 
+        if (nextQuestion.correctAnswer == index)  {
+            showAnswer.textContent = "Correct!";
+            answerDiv.innerHTML = " ";
+        }
+
+        questionIndex++;
+        showQuestion();
+        
+    }); 
+    
+        listItem.appendChild(answerButton); // appends button to list item
+        answerList.appendChild(listItem); // appends list item to answer div
+    }
+    
+        
+    
+
+    answerDiv.appendChild(answerList);
+    
+};
+
+
 
 function stopQuiz() {
     
@@ -132,10 +144,14 @@ function saveScore() {
 }
 
 function getScores() {
-
+    // scores = JSON.parse(localStorage.getItem("scores" || []));
 }
 
 
-// * on-click actions
+// * on-click events
+// One to start quiz: hides intro screen and starts timer
+// One to submit high scores and save to local storage
+// Go back: returns to intro screen
+// Clear high scores: clears high scores from local storage
 start.addEventListener("click", startQuiz);
 
