8e93bcd330430b33bc3cf1a8d66077a120ab2fda
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-07-01 22:15:22 -0600

    updated code up to function show high scores

--- Assets/script.js
+++ Assets/script.js
@@ -1,56 +1,58 @@
 // * Variables
+// intro and header
 var start = document.querySelector("#start-button");
 var mainText = document.querySelector("#main-text")
-var timeEl = document.querySelector("#timer");
 var intro = document.querySelector("#intro");
-var scorePage = document.querySelector("#scorepage")
-var questionAnswer = document.querySelector("#q-and-a");
 var viewScores = document.querySelector("#viewHS")
+// timer
+var timeEl = document.querySelector("#timer");
+var timeInterval;
+var secondsLeft = 75;
+// quiz
+var questionAnswer = document.querySelector("#q-and-a");
+var questionIndex = 0;
+var answerDiv = document.querySelector("#answers");
+// enter scores
+var scorePage = document.querySelector("#scorepage")
 var userScore = document.querySelector("#userscore");
-var userInput = document.querySelector("#name").value;
-var highScores = document.querySelector("#highscores");
-var scores = [];
 var submit = document.querySelector("#submit");
+
+
+// view high scores
+var highScoresDiv = document.querySelector("#highscores");
 var goBack = document.querySelector("#goback");
 var clearScores = document.querySelector("#clearscores");
- 
-
-var secondsLeft = 75;
-var questionIndex = 0;
-var timeInterval;
+var highScoresArray;
 
 
 // * Questions array
 
-var questions = [
+var questions = [{
+        question: "How do you join an array into a string?",
+        answer: [".join()", "join", "connect", "you can't"],
+        correctAnswer: 0,
+    },
     {
-    question: "How do you join an array into a string?",
-    answer: [".join()", "join", "connect", "you can't"],
-    correctAnswer: 0,
-},
-{
-    question: "What is the HTML tag that will output the largest header?",
-    answer: ["h6", "h3", "h1", "h0"],
-    correctAnswer: "2",
-},
-{
-    question: "How do you indicate an id in CSS",
-    answer: [".", "id", "-", "#"],
-    correctAnswer: "3",
-},
-{
-    question: "How do you start an HTML document?",
-    answer: ["html", "<!Doctype>", "!Doctype", "main"],
-    correctAnswer: "1",
-},
-{
-    question: "What character points to the entire document in CSS?",
-    answer: ["*", "-", "$", "~"],
-    correctAnswer: "0",
-}
+        question: "What is the HTML tag that will output the largest header?",
+        answer: ["h6", "h3", "h1", "h0"],
+        correctAnswer: 2,
+    },
+    {
+        question: "How do you indicate an id in CSS",
+        answer: [".", "id", "-", "#"],
+        correctAnswer: 3,
+    },
+    {
+        question: "How do you start an HTML document?",
+        answer: ["html", "<!Doctype>", "!Doctype", "main"],
+        correctAnswer: 1,
+    },
+    {
+        question: "What character points to the entire document in CSS?",
+        answer: ["*", "-", "$", "~"],
+        correctAnswer: 0,
+    }
 ]
-
-
 
 // * Functions
 // startTimer: begins timer at 75 seconds (secondsLeft) and counts down by 1 and stops at 0
@@ -60,156 +62,184 @@ var questions = [
 // get scores: display high scores from local storage
 // save scores: saves scores to local storage
 
-// * Step 1
-function startTimer() {
-    
-    timeInterval = setInterval(function() {
+
+function startTimer() { // * working
+
+    timeInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = "Timer: " + secondsLeft;
 
-        if (secondsLeft <= 0) {
+        if (secondsLeft <= 0 || questionIndex === -1) {
             stopTimer();
             stopQuiz();
-            questionAnswer.classList.add("hidden");
-            scorePage.classList.remove("hidden");
+            
         }
- 
-    
+
+
     }, 1000)
 
 }
 
-function stopTimer() {
-            
+function stopTimer() { // * working
+
     clearInterval(timeInterval);
-    
+
 }
 
-function startQuiz() {
+function startQuiz() { // * working
     intro.classList.add("hidden");
-    
+
+    getScores();
     startTimer();
     showQuestion();
 }
 
 
-
-function showQuestion() {
+function showQuestion() { // * working 
     var nextQuestion = questions[questionIndex];
     var questionDiv = document.querySelector("#question");
-    var answerDiv = document.querySelector("#answers");
-    var showAnswer = document.querySelector("#showAnswer");
+
     var answerList = document.createElement("ul");
     var listItem;
     var answerButton;
 
-    
-    
-    
+    questionDiv.textContent = nextQuestion.question;
+
+    // loops through each question
     for (var i = 0; i < nextQuestion.answer.length; i++) {
         listItem = document.createElement("li"); // create list item
         answerButton = document.createElement("button"); // create button that is inside list item
         answerButton.setAttribute("data-index", i); // gives each list item an index
         answerButton.textContent = nextQuestion.answer[i]; // populates button with each answer in array
 
-            // displays each question 
+        // displays each question 
         if (questionIndex < questions.length) {
-        questionDiv.textContent = nextQuestion.question;
+            questionDiv.textContent = nextQuestion.question;
         }
-
-        
 
         // checks if answer is correct when user clicks on button
-        answerButton.addEventListener("click",  function(event) {
-            var el = event.target;
-            var index = el.getAttribute("data-index");
-            
-         // conditionals for correct/incorrect answers   
-        if (nextQuestion.correctAnswer !== index) {
-            showAnswer.textContent = "Wrong";
-            secondsLeft-=5;
-            answerDiv.innerHTML = " "; // clears answers for next question
-        } 
-        if (nextQuestion.correctAnswer == index)  {
-            showAnswer.textContent = "Correct!";
-            answerDiv.innerHTML = " ";
-        }
-        
-    
+        answerButton.addEventListener("click", function (event) {
+            event.preventDefault();
+            checkAnswer(event, nextQuestion);
+        });
 
-        questionIndex++;
-       
-        // loops through questions
-        if(questionIndex < questions.length) {
-            showQuestion();
-            
-        } 
-        if (questionIndex === questions.length) {
-            stopQuiz();
-            }
-    }); 
-    
-    
-    listItem.appendChild(answerButton); // appends button to <li>
-    answerList.appendChild(listItem); // appends list item to <ul>
-}
+
+        listItem.appendChild(answerButton); // appends button to <li>
+        answerList.appendChild(listItem); // appends list item to <ul>
+    }
 
     answerDiv.appendChild(answerList); // appends list into answer div
+    questionIndex++; // update questions index
 
 };
 
+function checkAnswer(event, nextQuestion) { // *working
+
+    var el = event.target;
+    var index = parseInt(el.getAttribute("data-index"));
+    var showAnswer = document.querySelector("#showAnswer");
 
 
-function stopQuiz() {
+    // conditionals for correct/incorrect answers   
+    if (nextQuestion.correctAnswer !== index) {
+        showAnswer.textContent = "Wrong";
+        secondsLeft -= 5;
+
+    } else {
+        showAnswer.textContent = "Correct!";
+
+    }
+
+    setTimeout(function () {
+        answerDiv.innerHTML = "";
+        // debugger;
+        if ((secondsLeft === 0)|| questionIndex === questions.length) {
+            stopQuiz();
+        } else {
+            showQuestion();
+        }
+
+    }, 1000);
+
+}
+
+
+
+function stopQuiz() { // *working
     intro.classList.add("hidden");
     questionAnswer.classList.add("hidden");
     scorePage.classList.remove("hidden");
     timeEl.classList.add("hidden");
+    stopTimer();
+
 
     userScore.textContent = "Your score is " + secondsLeft;
 
 }
 
-function storeScore() {
-    var li = document.createElement("li");
-    li.textContent = userInput + secondsLeft;
 
-    highScores.appendChild(li);
+function saveScore() { //* working
+    var userInput = document.querySelector("#name").value;
+    var newScore = {
+        name: userInput,
+        score: secondsLeft
+    };
+    
+    highScoresArray.push(newScore);
+    localStorage.setItem("highScores", JSON.stringify(highScoresArray));
 }
 
-function getScores() {
-    localStorage.getItem("scores" || 0);
+
+function getScores() { // *working
+
+    highScoresArray = JSON.parse(localStorage.getItem("highScores")) || [];
+
+}
+
+function showHighScores() {
+    var hallOfFame = document.querySelector("#halloffame");
+    debugger;
+    for (var i = 0; i < highScoresArray.length; i++) {
+        var score = highScoresArray[i];
+        var li = document.createElement("li");
+        li.textContent = score;
+        hallOfFame.appendChild(li);
+    }
+
+    
 }
 
 
 // * on-click events
-// One to start quiz: hides intro screen and starts timer
-// One to submit high scores and save to local storage
-// Go back: returns to intro screen
-// Clear high scores: clears high scores from local storage
-start.addEventListener("click", startQuiz);
+// start quiz: hides intro screen and starts timer
+start.addEventListener("click", startQuiz); //* working
 
-viewScores.addEventListener("click", function () {
+// click "view high scores" takes you to high scores page
+viewScores.addEventListener("click", function () { //* working
     intro.classList.add("hidden");
-    highScores.classList.remove("hidden");
+    highScoresDiv.classList.remove("hidden");
+    getScores();
+    showHighScores();
 });
 
-
-// 
-submit.addEventListener("click", function(event) {
+// add scores to local storage on click of submit button
+submit.addEventListener("click", function (event) {
     event.preventDefault();
 
-    
-    localStorage.setItem("userInput", userInput);
-    
-    highScores.classList.remove("hidden");
+    saveScore();
+
+    highScoresDiv.classList.remove("hidden");
     scorePage.classList.add("hidden");
+
+});
+
+// Clear high scores: clears high scores from local storage
+clearScores.addEventListener("click", function () {
     
 });
 
-goBack.addEventListener("click", function() {
+// Go back: returns to intro screen
+goBack.addEventListener("click", function () { //* working
     intro.classList.remove("hidden");
-    highScores.classList.add("hidden");
-
+    highScoresDiv.classList.add("hidden");
 });
-
